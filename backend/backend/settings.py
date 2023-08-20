09c293122156d13b78cf28122d0beaebb479286a
Алина Утеулина <a.uteulina@mail.ru>
2023-08-20 20:51:43 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,8 +9,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-j_89af+30&&4qm*8z9_('
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['192.168.0.149', '255.255.0.0', '192.168.255.255', '158.160.67.16',
-                 '127.0.0.1', 'localhost', 'taskiuteulina.hopto.org']
+ALLOWED_HOSTS = ['192.168.0.149', '255.255.0.0', '192.168.255.255',
+                 '158.160.67.16', '127.0.0.1', 'localhost', 'taskiuteulina.hopto.org']
 
 
 # Application definition
